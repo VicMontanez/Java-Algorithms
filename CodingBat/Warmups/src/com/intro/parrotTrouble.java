e8abec78a34ce66c8c074654aa3a3d48703d5832
VicMontanez <v_montanez@hotmail.com>
2020-04-03 00:11:23 -0400

    completed algorithm

--- CodingBat/Warmups/src/com/intro/parrotTrouble.java
+++ CodingBat/Warmups/src/com/intro/parrotTrouble.java
@@ -1,4 +1,10 @@
 package com.intro;
 
 public class parrotTrouble {
+    public boolean parrotTrouble(boolean talking, int hour) {
+        if (talking == true && (hour < 7 || hour > 20)) {
+            return true;
+        }
+        return false;
+    }
 }

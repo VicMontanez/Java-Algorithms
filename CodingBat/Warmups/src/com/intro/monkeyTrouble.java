cebffbf00b56eec41ada43727134c5d95199b859
VicMontanez <v_montanez@hotmail.com>
2020-04-02 23:19:39 -0400

    completed algorithm

--- CodingBat/Warmups/src/com/intro/monkeyTrouble.java
+++ CodingBat/Warmups/src/com/intro/monkeyTrouble.java
@@ -1,4 +1,10 @@
 package com.intro;
 
 public class monkeyTrouble {
+    public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+        if ((aSmile && bSmile) || (!aSmile && !bSmile)) {
+            return true;
+        }
+        return false;
+    }
 }

5f1c69f2eaab01f74d05d6ea387ae3d7ac29dee9
VicMontanez <v_montanez@hotmail.com>
2020-04-04 13:52:17 -0400

    added file and problem

---
+++ CodingBat/Strings/src/com/intro/firstHalf.java
@@ -0,0 +1,11 @@
+//Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
+//
+//
+//        firstHalf("WooHoo") → "Woo"
+//        firstHalf("HelloThere") → "Hello"
+//        firstHalf("abcdef") → "abc"
+
+package com.intro;
+
+public class firstHalf {
+}

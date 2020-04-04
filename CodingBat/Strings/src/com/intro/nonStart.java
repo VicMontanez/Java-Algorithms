3f06780adcfec6555fefafa2e055c0b9866411d3
VicMontanez <v_montanez@hotmail.com>
2020-04-04 15:54:25 -0400

    solved algorithm

--- CodingBat/Strings/src/com/intro/nonStart.java
+++ CodingBat/Strings/src/com/intro/nonStart.java
@@ -8,4 +8,9 @@
 package com.intro;
 
 public class nonStart {
+    public String nonStart(String a, String b) {
+        String newA = a.substring(1);
+        String newB = b.substring(1);
+        return newA + newB;
+    }
 }

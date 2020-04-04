6bc9d86bf854940e77b1d52a00fa5e347b4aa314
VicMontanez <v_montanez@hotmail.com>
2020-04-04 15:43:44 -0400

    solved algorithm

--- CodingBat/Strings/src/com/intro/comboString.java
+++ CodingBat/Strings/src/com/intro/comboString.java
@@ -8,4 +8,12 @@
 package com.intro;
 
 public class comboString {
+    public String comboString(String a, String b) {
+        if (a.length() > b.length()) {
+            return b + a + b;
+        }
+
+        return a + b + a;
+    }
+
 }

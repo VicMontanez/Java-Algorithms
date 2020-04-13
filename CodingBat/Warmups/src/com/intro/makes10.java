f0fc4db34e7567aac440c8b915d13eeb43d9b224
VicMontanez <v_montanez@hotmail.com>
2020-04-12 22:35:54 -0400

    completed solution

--- CodingBat/Warmups/src/com/intro/makes10.java
+++ CodingBat/Warmups/src/com/intro/makes10.java
@@ -1,5 +1,13 @@
+//Given 2 ints, a and b, return true if one if them is 10 or if their sum is 10.
+
 package com.intro;
 
 public class makes10 {
+    public boolean makes10(int a, int b) {
+        if (a == 10 || b == 10 || a + b ==10) {
+            return true;
+        }
+        return false;
+    }
 
 }

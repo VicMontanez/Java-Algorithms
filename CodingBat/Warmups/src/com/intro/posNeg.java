a174953fdcfafba3f90b6f696a9604895864b485
VicMontanez <v_montanez@hotmail.com>
2020-04-14 00:13:11 -0400

    added solution

--- CodingBat/Warmups/src/com/intro/posNeg.java
+++ CodingBat/Warmups/src/com/intro/posNeg.java
@@ -3,4 +3,14 @@
 package com.intro;
 
 public class posNeg {
+    public boolean posNeg(int a, int b, boolean negative) {
+        if (negative) {
+            return (a < 0 && b < 0);
+        }
+        else if ((a <= 0 && b > 0) || (b <= 0 && a > 0)) {
+            return true;
+        }
+        return false;
+    }
+
 }

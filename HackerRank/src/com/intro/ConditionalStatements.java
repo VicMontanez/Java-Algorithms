1184202093083ecf318e5351ea78bca82446cd56
VicMontanez <v_montanez@hotmail.com>
2020-04-19 00:26:22 -0400

    added solution

--- HackerRank/src/com/intro/ConditionalStatements.java
+++ HackerRank/src/com/intro/ConditionalStatements.java
@@ -7,6 +7,39 @@
 //Complete the stub code provided in your editor to print whether or not n is weird.
 
 package com.intro;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
 
 public class ConditionalStatements {
+
+
+    public class Solution {
+
+
+
+        private static final Scanner scanner = new Scanner(System.in);
+
+        public static void main(String[] args) {
+            int N = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            scanner.close();
+
+            if (N % 2 == 1) {
+                System.out.println("Weird");
+            }
+            else if (N >= 6 && N <= 20) {
+                System.out.println("Weird");
+            }
+            else {
+                System.out.println("Not Weird");
+            }
+        }
+    }
+
 }

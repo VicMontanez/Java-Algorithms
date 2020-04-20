9194b4f7442d58a7dbd92d6eca92194d64cb547b
VicMontanez <v_montanez@hotmail.com>
2020-04-19 23:50:52 -0400

    added solution

--- HackerRank/src/com/intro/ClassVsInstance.java
+++ HackerRank/src/com/intro/ClassVsInstance.java
@@ -8,6 +8,56 @@
 //To help you learn by example and complete this challenge, much of the code is provided for you, but you'll be writing everything in the future. The code that creates each instance of your Person class is in the main method. Don't worry if you don't understand it all quite yet!
 
 package com.intro;
+import java.io.*;
+import java.util.*;
 
 public class ClassVsInstance {
+    public class Person {
+        private int age;
+
+        public Person(int initialAge) {
+            // Add some more code to run some checks on initialAge
+            if(initialAge > 0) {
+                this.age = initialAge;
+            }
+            else {
+                System.out.println("Age is not valid, setting age to 0.");
+                this.age = 0;
+            }
+        }
+
+        public void amIOld() {
+            // Write code determining if this person's age is old and print the correct statement:
+            if(age < 13) {
+                System.out.println("You are young.");
+            }
+            else if (age >= 13 && age < 18) {
+                System.out.println("You are a teenager.");
+            }
+            else {
+                System.out.println("You are old.");
+            }
+        }
+
+        public void yearPasses() {
+            // Increment this person's age.
+            this.age++;
+        }
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int T = sc.nextInt();
+            for (int i = 0; i < T; i++) {
+                int age = sc.nextInt();
+                Person p = new Person(age);
+                p.amIOld();
+                for (int j = 0; j < 3; j++) {
+                    p.yearPasses();
+                }
+                p.amIOld();
+                System.out.println();
+            }
+            sc.close();
+        }
+    }
 }

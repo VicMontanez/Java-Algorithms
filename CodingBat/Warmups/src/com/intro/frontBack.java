359f10d50f2196aa5c41ac6ea834875a6a23651b
VicMontanez <v_montanez@hotmail.com>
2020-04-23 20:34:33 -0400

    added solution

--- CodingBat/Warmups/src/com/intro/frontBack.java
+++ CodingBat/Warmups/src/com/intro/frontBack.java
@@ -3,4 +3,15 @@
 package com.intro;
 
 public class frontBack {
+    public String frontBack(String str) {
+        if(str.length() <= 1) {
+            return str;
+        }
+        String front = str.substring(0,1);
+        String back = str.substring(str.length()-1);
+        String middle = str.substring(1, (str.length()-1));
+
+        return back + middle + front;
+    }
+
 }

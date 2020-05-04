d33864384cd0328a7f1131ee3519435b2d34928b
VicMontanez <v_montanez@hotmail.com>
2020-05-03 20:54:00 -0400

    added solution

--- CodingBat/Strings/src/com/intro/right2.java
+++ CodingBat/Strings/src/com/intro/right2.java
@@ -3,4 +3,11 @@
 package com.intro;
 
 public class right2 {
+    public String right2(String str) {
+        String lastTwo = str.substring(str.length() - 2);
+        String start = str.substring(0, str.length() - 2);
+
+        return lastTwo + start;
+    }
+
 }

9211ba882047464aa6535078af7f6e626349e7fa
VicMontanez <v_montanez@hotmail.com>
2020-05-12 22:15:04 -0400

    added solution

--- CodingBat/Warmups/src/com/intro/in1020.java
+++ CodingBat/Warmups/src/com/intro/in1020.java
@@ -2,4 +2,13 @@
 package com.intro;
 
 public class in1020 {
+    public boolean in1020(int a, int b) {
+        if ((a >= 10 &&a <=20) || (b >= 10 && b <= 20)) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
 }

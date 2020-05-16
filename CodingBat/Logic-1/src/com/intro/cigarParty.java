523ee4b326e58fc63e7d274c3dfdbcd31d87bfb1
VicMontanez <v_montanez@hotmail.com>
2020-05-15 23:12:48 -0400

    added solution

--- CodingBat/Logic-1/src/com/intro/cigarParty.java
+++ CodingBat/Logic-1/src/com/intro/cigarParty.java
@@ -3,4 +3,14 @@
 package com.intro;
 
 public class cigarParty {
+    public boolean cigarParty(int cigars, boolean isWeekend) {
+        if (isWeekend && cigars >= 40) {
+            return true;
+        } else if (cigars >= 40 && cigars <= 60) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }

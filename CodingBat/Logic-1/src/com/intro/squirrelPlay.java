efbee13058a458c451ced859ae5973427f6863ce
VicMontanez <v_montanez@hotmail.com>
2020-05-20 23:19:05 -0400

    solved algorithm

--- CodingBat/Logic-1/src/com/intro/squirrelPlay.java
+++ CodingBat/Logic-1/src/com/intro/squirrelPlay.java
@@ -3,4 +3,17 @@
 package com.intro;
 
 public class squirrelPlay {
+
+    public boolean squirrelPlay(int temp, boolean isSummer) {
+        if(!isSummer && (temp >=60 && temp <= 90)) {
+            return true;
+        } else if(isSummer && (temp >=60 && temp <=100)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+
 }

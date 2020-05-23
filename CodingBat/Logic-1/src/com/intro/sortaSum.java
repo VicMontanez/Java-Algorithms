c657be74f7a6a53bddc3fcf99e12a4055a5b0635
VicMontanez <v_montanez@hotmail.com>
2020-05-23 00:09:07 -0400

    completed algorithm

--- CodingBat/Logic-1/src/com/intro/sortaSum.java
+++ CodingBat/Logic-1/src/com/intro/sortaSum.java
@@ -3,4 +3,12 @@
 package com.intro;
 
 public class sortaSum {
+    public int sortaSum(int a, int b) {
+        int sum = a + b;
+        if (sum >= 10 && sum <= 19) {
+            return 20;
+        }
+        return sum;
+    }
+
 }

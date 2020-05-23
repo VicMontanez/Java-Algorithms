529c82d6c4f68a1c06057ac17864c82f9c237626
VicMontanez <v_montanez@hotmail.com>
2020-05-22 23:37:51 -0400

    added file & problem explaination

---
+++ CodingBat/Logic-1/src/com/intro/sortaSum.java
@@ -0,0 +1,6 @@
+//Given 2 ints, a and b, return their sum. However, sums in the range 10..19 inclusive, are forbidden, so in that case just return 20.
+
+package com.intro;
+
+public class sortaSum {
+}

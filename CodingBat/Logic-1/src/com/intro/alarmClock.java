c0122edc690e1af6edbb207b22e38669d7f4c75f
VicMontanez <v_montanez@hotmail.com>
2020-06-03 23:34:15 -0400

    added solution

--- CodingBat/Logic-1/src/com/intro/alarmClock.java
+++ CodingBat/Logic-1/src/com/intro/alarmClock.java
@@ -3,4 +3,13 @@
 package com.intro;
 
 public class alarmClock {
+    public String alarmClock(int day, boolean vacation) {
+        if ( vacation == true && ( day == 0 || day == 6 ) )
+            return "off";
+        if ( vacation == true && ( day == 1 || day == 2 || day == 3 || day == 4 || day == 5 ) )
+            return "10:00";
+        if ( vacation == false && ( day == 0 || day == 6 ) )
+            return "10:00";
+        else return "7:00";
+    }
 }

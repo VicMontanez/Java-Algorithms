8459ec992805de118ab34ae807d3e1c47e1e3afe
VicMontanez <v_montanez@hotmail.com>
2020-06-06 22:56:27 -0400

    attempt to complete algorithm

--- CodingBat/Logic-1/src/com/intro/love6.java
+++ CodingBat/Logic-1/src/com/intro/love6.java
@@ -3,4 +3,11 @@
 package com.intro;
 
 public class love6 {
+    public boolean love6(int a, int b) {
+        if ( a == 6 || b == 6 || Math.abs( a - b ) == 6 || Math.abs( a + b ) == 6)
+            return true;
+        else {
+            return false;
+        }
+    }
 }

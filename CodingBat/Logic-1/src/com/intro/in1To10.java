9950d8266b746abd56e84a289a4562b732a6b27b
VicMontanez <v_montanez@hotmail.com>
2020-06-11 17:49:27 -0400

    added solution

--- CodingBat/Logic-1/src/com/intro/in1To10.java
+++ CodingBat/Logic-1/src/com/intro/in1To10.java
@@ -3,4 +3,11 @@
 package com.intro;
 
 public class in1To10 {
+    public boolean in1To10(int n, boolean outsideMode) {
+        if ( outsideMode == false && n >= 1 && n <= 10 )
+            return true;
+        if ( outsideMode == true && ( n <= 1 || n >= 10 ) )
+            return true;
+        else return false;
+    }
 }

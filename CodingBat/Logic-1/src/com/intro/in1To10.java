8e399e2402ec010ce98cbf37ba415ba26e6aea5b
VicMontanez <v_montanez@hotmail.com>
2020-06-11 17:48:56 -0400

    added file and description

---
+++ CodingBat/Logic-1/src/com/intro/in1To10.java
@@ -0,0 +1,6 @@
+//Given a number n, return true if n is in the range 1..10, inclusive. Unless outsideMode is true, in which case return true if the number is less or equal to 1, or greater or equal to 10.
+
+package com.intro;
+
+public class in1To10 {
+}

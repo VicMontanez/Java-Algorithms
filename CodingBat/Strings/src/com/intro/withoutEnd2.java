0ed1309ab843247b549e76f3d485c604abd87355
VicMontanez <v_montanez@hotmail.com>
2020-06-14 21:17:53 -0400

    added solution

--- CodingBat/Strings/src/com/intro/withoutEnd2.java
+++ CodingBat/Strings/src/com/intro/withoutEnd2.java
@@ -3,4 +3,13 @@
 package com.intro;
 
 public class withoutEnd2 {
+    public String withouEnd2(String str) {
+        if (str.length() < 3) {
+            return "";
+        }
+        else  {
+            return str.substring(1, str.length() - 1);
+        }
+    }
+
 }

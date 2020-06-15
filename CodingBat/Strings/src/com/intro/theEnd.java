88a83feb4a765449dbec6496683cfa5216cff487
VicMontanez <v_montanez@hotmail.com>
2020-06-14 21:01:28 -0400

    added solution

--- CodingBat/Strings/src/com/intro/theEnd.java
+++ CodingBat/Strings/src/com/intro/theEnd.java
@@ -3,4 +3,12 @@
 package com.intro;
 
 public class theEnd {
+    public String theEnd(String str, boolean front) {
+        if (front == true) {
+            return str.substring(0,1);
+        } else{
+            return str.substring(str.length() - 1);
+        }
+    }
+
 }

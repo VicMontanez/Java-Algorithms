645fce3440de5a110f0f11981cea7941cf72987c
VicMontanez <v_montanez@hotmail.com>
2020-06-14 21:17:14 -0400

    added file and explaination

---
+++ CodingBat/Strings/src/com/intro/withoutEnd2.java
@@ -0,0 +1,6 @@
+//Given a string, return a version without both the first and last char of the string. The string may be any length, including 0.
+
+package com.intro;
+
+public class withoutEnd2 {
+}

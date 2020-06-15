89eafdaff53e7ab2250f54728609b8b5bd4d29e9
VicMontanez <v_montanez@hotmail.com>
2020-06-14 21:00:33 -0400

    added file and problem explaination

---
+++ CodingBat/Strings/src/com/intro/theEnd.java
@@ -0,0 +1,6 @@
+//Given a string, return a string length 1 from its front, unless front is false, in which case return a string length 1 from its back. The string will be non-empty.
+
+package com.intro;
+
+public class theEnd {
+}

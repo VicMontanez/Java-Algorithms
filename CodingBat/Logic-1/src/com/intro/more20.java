1df8d281560384157c4f5ef32de64a6d9e2a2716
VicMontanez <v_montanez@hotmail.com>
2020-07-04 12:36:28 -0400

    added solution

--- CodingBat/Logic-1/src/com/intro/more20.java
+++ CodingBat/Logic-1/src/com/intro/more20.java
@@ -3,4 +3,12 @@
 package com.intro;
 
 public class more20 {
+    public boolean more20(int n) {
+        if (n % 20 == 1 || n % 20 == 2) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }

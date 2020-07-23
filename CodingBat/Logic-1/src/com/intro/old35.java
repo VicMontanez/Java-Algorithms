3e77171c007c2017c00a33ffa8ef6ffea0f3c7fc
VicMontanez <v_montanez@hotmail.com>
2020-07-22 23:18:59 -0400

    added solution

--- CodingBat/Logic-1/src/com/intro/old35.java
+++ CodingBat/Logic-1/src/com/intro/old35.java
@@ -3,4 +3,12 @@
 package com.intro;
 
 public class old35 {
+    public boolean old35(int n) {
+        if (n % 3 == 0 ^ n % 5 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }

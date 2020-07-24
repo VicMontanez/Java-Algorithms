a01468f7c4c6e9ac14e6ec5266ed9ac9623707b1
VicMontanez <v_montanez@hotmail.com>
2020-07-23 23:40:25 -0400

    added solution

--- HackerRank/src/com/intro/Arrays.java
+++ HackerRank/src/com/intro/Arrays.java
@@ -1,6 +1,43 @@
-//Given an array, , of  integers, print 's elements in reverse order as a single line of space-separated numbers.
+//Given an array, of  integers, print 's elements in reverse order as a single line of space-separated numbers.
 
 package com.intro;
 
+import java.util.Scanner;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
 public class Arrays {
+
+
+
+    public class Solution  {
+
+
+
+        private static final Scanner scanner = new Scanner(System.in);
+
+        public static void main(String[] args) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int[] arr = new int[n];
+
+            String[] arrItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = n - 1; i >= 0; i--) {
+                int arrItem = Integer.parseInt(arrItems[i]);
+                arr[i] = arrItem;
+                System.out.print(arr[i] + " ");
+            }
+
+            scanner.close();
+        }
+    }
+
 }

53a10e9a4d50e2cf4d5e70582fe96be4d2355a2c
VicMontanez <v_montanez@hotmail.com>
2020-07-25 13:09:47 -0400

    added problem explaination

--- HackerRank/src/com/intro/LetsReview.java
+++ HackerRank/src/com/intro/LetsReview.java
@@ -1,4 +1,4 @@
-
+//Given a string, , of length  that is indexed from  to , print its even-indexed and odd-indexed characters as  space-separated strings on a single line (see the Sample below for more detail).
 
 package com.intro;
 

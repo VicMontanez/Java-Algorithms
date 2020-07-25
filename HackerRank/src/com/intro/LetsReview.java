a2a714776a78c41d1f5392b7aff8b69e3fc76c20
VicMontanez <v_montanez@hotmail.com>
2020-07-25 13:10:26 -0400

    added solution

--- HackerRank/src/com/intro/LetsReview.java
+++ HackerRank/src/com/intro/LetsReview.java
@@ -3,4 +3,41 @@
 package com.intro;
 
 public class LetsReview {
+    import java.io.*;
+import java.util.*;
+import java.util.Scanner;  // Import the Scanner class
+
+    public class Solution {
+
+        public static void main(String[] args) {
+            /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+            Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+
+            int age = myObj.nextInt();
+            // String name = myObj.nextLine();
+            // String name2 = myObj.nextLine();
+
+            for(int c=0;c<age;c++) {
+                String even = "";
+                String odd = "";
+                String myString = myObj.next();
+                char[] charArray = myString.toCharArray();
+                for (int i = 0; i < charArray.length; i += 2) {
+                    even += charArray[i];
+                }
+                for (int j = 1; j < charArray.length; j += 2) {
+                    odd += charArray[j];
+                }
+                System.out.println(even + " " + odd);
+            }
+
+            // Numerical input
+
+            // System.out.println(age);
+            // System.out.println(name);
+            // System.out.println(name2);
+
+        }
+    }
+
 }

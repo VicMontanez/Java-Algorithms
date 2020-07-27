ebd560cf64dc34f28b9e78147f065d2195c3438b
VicMontanez <v_montanez@hotmail.com>
2020-07-26 22:37:56 -0400

    added problem explaination

--- CodingBat/Logic-1/src/com/intro/less20.java
+++ CodingBat/Logic-1/src/com/intro/less20.java
@@ -1,3 +1,5 @@
+//Return true if the given non-negative number is 1 or 2 less than a multiple of 20. So for example 38 and 39 return true, but 40 returns false.
+
 package com.intro;
 
 public class less20 {

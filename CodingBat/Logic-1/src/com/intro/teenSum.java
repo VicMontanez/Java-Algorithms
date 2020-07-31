b1b939bf0236be75881528ce9b877319ab678f83
VicMontanez <v_montanez@hotmail.com>
2020-07-30 22:20:41 -0400

    added solution

--- CodingBat/Logic-1/src/com/intro/teenSum.java
+++ CodingBat/Logic-1/src/com/intro/teenSum.java
@@ -3,4 +3,14 @@
 package com.intro;
 
 public class teenSum {
+    public int teenSum(int a, int b) {
+
+        if (( a >=13 && a <= 19) || (b >= 13 && b <=19)) {
+            return 19;
+        } else {
+            return sum;
+        }
+
+    }
+
 }

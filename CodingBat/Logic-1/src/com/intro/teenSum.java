8cb7ebe6039e47ad97f8b7955e6e0a3e5cd60541
VicMontanez <v_montanez@hotmail.com>
2020-07-30 22:21:18 -0400

    fixed error

--- CodingBat/Logic-1/src/com/intro/teenSum.java
+++ CodingBat/Logic-1/src/com/intro/teenSum.java
@@ -4,7 +4,7 @@ package com.intro;
 
 public class teenSum {
     public int teenSum(int a, int b) {
-
+        int sum = a + b;
         if (( a >=13 && a <= 19) || (b >= 13 && b <=19)) {
             return 19;
         } else {

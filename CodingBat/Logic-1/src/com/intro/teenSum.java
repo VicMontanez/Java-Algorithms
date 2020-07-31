5ef172def02c0c06690471ae3f56c7fdc2dbc536
VicMontanez <v_montanez@hotmail.com>
2020-07-30 22:04:13 -0400

    added problem explaination

--- CodingBat/Logic-1/src/com/intro/teenSum.java
+++ CodingBat/Logic-1/src/com/intro/teenSum.java
@@ -1,3 +1,5 @@
+//Given 2 ints, a and b, return their sum. However, "teen" values in the range 13..19 inclusive, are extra lucky. So if either value is a teen, just return 19.
+
 package com.intro;
 
 public class teenSum {

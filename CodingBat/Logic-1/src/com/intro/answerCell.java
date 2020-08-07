680000412bf817f11ad171f5875e96d8dc4a15da
VicMontanez <v_montanez@hotmail.com>
2020-08-07 10:35:27 -0400

    added problem explaination

--- CodingBat/Logic-1/src/com/intro/answerCell.java
+++ CodingBat/Logic-1/src/com/intro/answerCell.java
@@ -1,3 +1,5 @@
+//Your cell phone rings. Return true if you should answer it. Normally you answer, except in the morning you only answer if it is your mom calling. In all cases, if you are asleep, you do not answer.
+
 package com.intro;
 
 public class answerCell {

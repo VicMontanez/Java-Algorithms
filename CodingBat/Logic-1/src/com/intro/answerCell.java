3723d509f169bb1536952bc131a763f34faebce4
VicMontanez <v_montanez@hotmail.com>
2020-08-08 12:48:32 -0400

    algorithm attempt

--- CodingBat/Logic-1/src/com/intro/answerCell.java
+++ CodingBat/Logic-1/src/com/intro/answerCell.java
@@ -3,4 +3,13 @@
 package com.intro;
 
 public class answerCell {
+    public boolean answerCell(boolean isMorning, boolean isMom, boolean isAsleep) {
+        if (isAsleep)
+        return false;
+        if ((isMorning && isMom) || !isMorning)
+        return false;
+         else return true;
+
+    }
+
 }

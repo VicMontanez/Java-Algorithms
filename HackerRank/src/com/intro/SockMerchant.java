ef62b400b8d910e6df5fa5fc686eeeca2ad9c946
VicMontanez <v_montanez@hotmail.com>
2020-08-09 13:11:19 -0400

    added solution

--- HackerRank/src/com/intro/SockMerchant.java
+++ HackerRank/src/com/intro/SockMerchant.java
@@ -1,4 +1,45 @@
 package com.intro;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
 
 public class SockMerchant {
+
+
+    public class Solution {
+
+        // https://www.geeksforgeeks.org/java-util-hashmap-in-java-with-examples/
+
+        // Complete the sockMerchant function below.
+        static int sockMerchant(int n, int[] ar) {
+            int pairs = 0;
+
+            // Create empty hashmap
+            HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+            // Add ar indexes to the map
+            for (int i = 0; i < ar.length; i++) {
+                if (!map.containsKey(ar[i])) {
+                    map.put(ar[i], 1);
+                } else {
+                    map.put(ar[i], map.get(ar[i]) + 1);
+                }
+            }
+
+            // System.out.println(map);
+
+            // Loop through the hashmap
+            for (Map.Entry socks : map.entrySet()) {
+                System.out.println(pairs += (int) socks.getValue() / 2);
+            }
+            return pairs;
+
+        }
+
+        private static final 
+    }
 }

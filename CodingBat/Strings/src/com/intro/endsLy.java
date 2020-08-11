f2c799f38eb5ac1cc3f7f4b88c76723b657c1c3f
VicMontanez <v_montanez@hotmail.com>
2020-08-10 23:53:18 -0400

    posted attempt

--- CodingBat/Strings/src/com/intro/endsLy.java
+++ CodingBat/Strings/src/com/intro/endsLy.java
@@ -3,4 +3,17 @@
 package com.intro;
 
 public class endsLy {
+    public boolean endsLy(String str) {
+        int length = str.length();
+        String lastTwo = str.substring(str.length() - 2);
+
+        if(length < 2) {
+            return false;
+        } else if(lastTwo.contains("ly")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }

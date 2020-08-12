f5ebbed9cdc3f2747c931509dd7468778e29fc86
VicMontanez <v_montanez@hotmail.com>
2020-08-11 23:24:34 -0400

    added problem explaination

--- CodingBat/Maps/src/com/intro/mapBully.java
+++ CodingBat/Maps/src/com/intro/mapBully.java
@@ -1,3 +1,6 @@
+//Modify and return the given map as follows: if the key "a" has a value, set the key "b" to have that value, and set the key "a" to have the value "". Basically "b" is a bully, taking the value and replacing it with the empty string.
+
+
 package com.intro;
 
 public class mapBully {

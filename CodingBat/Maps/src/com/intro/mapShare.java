15a79ce1efc6e0403b6fee6e432636824604ab40
VicMontanez <v_montanez@hotmail.com>
2020-08-13 00:14:59 -0400

    added solution

--- CodingBat/Maps/src/com/intro/mapShare.java
+++ CodingBat/Maps/src/com/intro/mapShare.java
@@ -5,4 +5,14 @@ package com.intro;
 import java.util.Map;
 
 public class mapShare {
+    public Map<String, String> mapShare(Map<String, String> map) {
+        if(map.containsKey("a")) {
+            map.put("b", map.get("a"));
+            map.remove("c");
+        } else {
+            map.remove("c");
+        }
+        return map;
+    }
+
 }

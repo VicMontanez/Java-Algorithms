a0884c0e235ffdefe3012d49818e56ded86c3094
VicMontanez <v_montanez@hotmail.com>
2020-08-12 23:51:27 -0400

    added problem explaination

--- CodingBat/Maps/src/com/intro/mapShare.java
+++ CodingBat/Maps/src/com/intro/mapShare.java
@@ -1,3 +1,5 @@
+//Modify and return the given map as follows: if the key "a" has a value, set the key "b" to have that same value. In all cases remove the key "c", leaving the rest of the map unchanged.
+
 package com.intro;
 
 public class mapShare {

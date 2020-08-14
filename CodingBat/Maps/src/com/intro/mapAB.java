1d6b42c15dadd062c67d61ae76b277566649acfb
VicMontanez <v_montanez@hotmail.com>
2020-08-14 01:24:05 -0400

    added problem explaination

--- CodingBat/Maps/src/com/intro/mapAB.java
+++ CodingBat/Maps/src/com/intro/mapAB.java
@@ -1,3 +1,5 @@
+//Modify and return the given map as follows: for this problem the map may or may not contain the "a" and "b" keys. If both keys are present, append their 2 string values together and store the result under the key "ab".
+
 package com.intro;
 
 public class mapAB {

0c050c24c1b451b7cbcead906c50695621c526f7
VicMontanez <v_montanez@hotmail.com>
2020-08-15 13:29:57 -0400

    added solution

--- CodingBat/Maps/src/com/intro/topping2.java
+++ CodingBat/Maps/src/com/intro/topping2.java
@@ -3,4 +3,14 @@
 package com.intro;
 
 public class topping2 {
+    public Map<String, String> topping2(Map<String, String> map) {
+        if(map.containsKey("ice cream")) {
+            map.put("yogurt", map.get("ice cream"));
+        }
+        if (map.containsKey("spinach")) {
+            map.put("spinach", "nuts");
+        }
+        return map;
+    }
+
 }

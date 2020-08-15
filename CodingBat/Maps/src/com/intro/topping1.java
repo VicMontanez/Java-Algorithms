6da518a56edc1fd851c13decd9c23e41d539e925
VicMontanez <v_montanez@hotmail.com>
2020-08-15 13:07:37 -0400

    added problem explaination

--- CodingBat/Maps/src/com/intro/topping1.java
+++ CodingBat/Maps/src/com/intro/topping1.java
@@ -1,3 +1,5 @@
+//Given a map of food keys and topping values, modify and return the map as follows: if the key "ice cream" is present, set its value to "cherry". In all cases, set the key "bread" to have the value "butter".
+
 package com.intro;
 
 public class topping1 {

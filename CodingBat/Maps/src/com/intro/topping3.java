d4f4bdb436ce9f48c47992dade031a44a2bdf3f4
VicMontanez <v_montanez@hotmail.com>
2020-08-16 21:57:26 -0400

    added problem explaination

--- CodingBat/Maps/src/com/intro/topping3.java
+++ CodingBat/Maps/src/com/intro/topping3.java
@@ -1,3 +1,5 @@
+//Given a map of food keys and topping values, modify and return the map as follows: if the key "potato" has a value, set that as the value for the key "fries". If the key "salad" has a value, set that as the value for the key "spinach".
+
 package com.intro;
 
 public class topping3 {

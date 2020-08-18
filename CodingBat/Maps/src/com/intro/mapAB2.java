1db49296dcf0896a23fdcf0dceb574592eeaf6b7
VicMontanez <v_montanez@hotmail.com>
2020-08-18 00:43:45 -0400

    added solution

--- CodingBat/Maps/src/com/intro/mapAB2.java
+++ CodingBat/Maps/src/com/intro/mapAB2.java
@@ -3,4 +3,16 @@
 package com.intro;
 
 public class mapAB2 {
+    public Map<String, String> mapAB2(Map<String, String> map) {
+        String a = map.get("a");
+        String b = map.get("b");
+        if(map.containsKey("a") && map.containsKey("b")) {
+            if (a.equals(b)) {
+                map.remove("a");
+                map.remove("b");
+            }
+        }
+        return map;
+    }
+
 }

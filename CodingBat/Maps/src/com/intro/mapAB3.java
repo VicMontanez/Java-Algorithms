a068edada5831e5cb052a3a20b757359d798eec8
VicMontanez <v_montanez@hotmail.com>
2020-08-19 23:15:48 -0400

    added solution

--- CodingBat/Maps/src/com/intro/mapAB3.java
+++ CodingBat/Maps/src/com/intro/mapAB3.java
@@ -6,7 +6,16 @@
 
 package com.intro;
 
-import java.util.HashMap;
+import java.util.Map;
 
 public class mapAB3 {
+    public Map<String, String> mapAB3(Map<String, String> map) {
+        if(map.containsKey("a") && !map.containsKey("b")){
+            map.put("b", map.get("a"));
+        } if (map.containsKey("b") && !map.containsKey("a")){
+            map.put("a", map.get("b"));
+        }
+        return map;
+    }
+
 }

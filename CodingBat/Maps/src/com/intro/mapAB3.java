9760a6c2ce70e182f25ea701450bf46f48368655
VicMontanez <v_montanez@hotmail.com>
2020-08-19 23:12:43 -0400

    added problem description

--- CodingBat/Maps/src/com/intro/mapAB3.java
+++ CodingBat/Maps/src/com/intro/mapAB3.java
@@ -1,3 +1,5 @@
+//Modify and return the given map as follows: if exactly one of the keys "a" or "b" has a value in the map (but not both), set the other to have that same value in the map.
+
 package com.intro;
 
 public class mapAB3 {

c689caaf1a0e59c34911d6f0c161b9f6e6b4a37f
VicMontanez <v_montanez@hotmail.com>
2020-08-23 15:28:26 -0400

    added solution

--- HackerRank/src/com/intro/BeautifulBinaryString.java
+++ HackerRank/src/com/intro/BeautifulBinaryString.java
@@ -1,4 +1,51 @@
 package com.intro;
 
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
 public class BeautifulBinaryString {
+
+    // Complete the beautifulBinaryString function below.
+    static int beautifulBinaryString(String b) {
+        // int counter = 0;
+        // int stringLength = b.length();
+        // String ugly = "010";
+
+        // for(int i =0; i<=stringLength - 3;) {
+        //     String testcase = b.substring(i, i + 3);
+        //     if(testcase.equals(ugly)) {
+        //         counter++;
+        //         i += 3;
+        //     } else {
+        //         i++;
+        //     }
+        // }
+
+        return b.length() - b.replaceAll("010", "11").length();
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String b = scanner.nextLine();
+
+        int result = beautifulBinaryString(b);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }

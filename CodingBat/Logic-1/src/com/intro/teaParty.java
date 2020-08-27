d8d90b455b9c93295a67d0291c756869f0cef27d
VicMontanez <v_montanez@hotmail.com>
2020-08-27 01:56:45 -0400

    added solution

--- CodingBat/Logic-1/src/com/intro/teaParty.java
+++ CodingBat/Logic-1/src/com/intro/teaParty.java
@@ -3,4 +3,14 @@
 package com.intro;
 
 public class teaParty {
+    public int teaParty(int tea, int candy) {
+        if((tea < 5) || (candy < 5))  {
+            return 0;
+        } if (((tea >= 2 * candy) || (candy >= 2 * tea))) {
+            return 2;
+        } else  {
+            return 1;
+        }
+    }
+
 }

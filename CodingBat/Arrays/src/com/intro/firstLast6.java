826016cc92d658f510043b9032032cf2c672155e
VicMontanez <v_montanez@hotmail.com>
2020-09-24 23:11:04 -0400

    added problem explaination

--- CodingBat/Arrays/src/com/intro/firstLast6.java
+++ CodingBat/Arrays/src/com/intro/firstLast6.java
@@ -1,3 +1,5 @@
+//Given an array of ints, return true if 6 appears as either the first or last element in the array. The array will be length 1 or more
+
 package com.intro;
 
 public class firstLast6 {

778c4b840d81ee81c6d1cf7707ca3e581d674dbe
VicMontanez <v_montanez@hotmail.com>
2020-09-24 23:48:17 -0400

    added solution

--- CodingBat/Arrays/src/com/intro/firstLast6.java
+++ CodingBat/Arrays/src/com/intro/firstLast6.java
@@ -3,4 +3,15 @@
 package com.intro;
 
 public class firstLast6 {
+    public boolean firstLast6(int[] nums) {
+        if(nums[0] == 6) {
+            return true;
+        }
+        if ((nums[nums.length - 1]) == 6) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }

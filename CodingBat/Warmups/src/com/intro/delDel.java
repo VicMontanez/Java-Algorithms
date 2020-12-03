0f03cf996295c32ab03986df8da442864c2017f4
VicMontanez <v_montanez@hotmail.com>
2020-12-02 22:21:05 -0500

    added solution

--- CodingBat/Warmups/src/com/intro/delDel.java
+++ CodingBat/Warmups/src/com/intro/delDel.java
@@ -3,4 +3,12 @@
 package com.intro;
 
 public class delDel {
+    public String delDel(String str) {
+        if (str.length() >= 4 && str.substring(1, 4).equals("del")) {
+            return str.substring(0, 1) + str.substring(4);
+        }
+    }
 }
+
+
+

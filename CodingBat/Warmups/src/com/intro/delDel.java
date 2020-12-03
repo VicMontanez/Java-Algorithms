6089d425cd1f725b0e0a1b2cca6e1aa503e28701
VicMontanez <v_montanez@hotmail.com>
2020-12-02 22:21:52 -0500

    added complete solution

--- CodingBat/Warmups/src/com/intro/delDel.java
+++ CodingBat/Warmups/src/com/intro/delDel.java
@@ -7,6 +7,7 @@ public class delDel {
         if (str.length() >= 4 && str.substring(1, 4).equals("del")) {
             return str.substring(0, 1) + str.substring(4);
         }
+        return str;
     }
 }
 
